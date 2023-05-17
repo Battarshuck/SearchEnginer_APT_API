12b9eb76912b8a28cd156a860db4a4a53a10f8d9
AmrAhmed412 <75176768+AmrAhmed412@users.noreply.github.com>
2023-05-17 06:00:08 +0300

    Stemming the Word list

--- src/main/java/com/example/demo/Websites/WebSiteService.java
+++ src/main/java/com/example/demo/Websites/WebSiteService.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.stereotype.Service;
+import com.example.demo.WordProcessor.WordProcessor;
 
 import java.util.List;
+
 @Service
 public class WebSiteService {
 
     SampleRepository sampleRepo;
+
     @Autowired
     public WebSiteService(SampleRepository sampleRepo) {
         this.sampleRepo = sampleRepo;
@@ -19,7 +22,11 @@ public class WebSiteService {
 
     public List<Samples> findByWordIn(String words) {
         List<String> listsWords = List.of(words.split("[,!.+/ ]+"));
-        //STEM-ING LOGIC
+        WordProcessor wp = new WordProcessor();
+        for (int i = 0; i < listsWords.size(); i++) { //for each word in the list
+            listsWords.set(i, listsWords.get(i).replaceAll("[^a-zA-Z]", "")); //only allowing alphanumerics
+            listsWords.set(i,wp.processWord(listsWords.get(i))); //setting word in list with the stem of the words 
+        }
         return sampleRepo.findByWordIn(listsWords);
     }
 
