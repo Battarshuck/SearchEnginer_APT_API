2c3ed006b01aa95aac5ccef5dd31c1fe0db3caac
AmrAhmed412 <75176768+AmrAhmed412@users.noreply.github.com>
2023-05-18 07:53:13 +0300

    phrase searching

--- src/main/java/com/example/demo/Websites/WebSiteService.java
+++ src/main/java/com/example/demo/Websites/WebSiteService.java
@@ -23,13 +23,14 @@ public class WebSiteService {
     Ranker ranker;
 
     @Autowired
-    public WebSiteService(SampleRepository sampleRepo, CrawlRepository crawlRepo, UnstemmedSampleRepository unstemmedSampleRepository, PRRObjectRepository prrObjectRepository) {
+    public WebSiteService(SampleRepository sampleRepo, CrawlRepository crawlRepo,
+            UnstemmedSampleRepository unstemmedSampleRepository, PRRObjectRepository prrObjectRepository) {
         this.sampleRepo = sampleRepo;
         this.crawlRepo = crawlRepo;
         this.unstemmedSampleRepository = unstemmedSampleRepository;
         this.prrObjectRepository = prrObjectRepository;
 
-        List<PRRObject> temp =  prrObjectRepository.findAllByRank();
+        List<PRRObject> temp = prrObjectRepository.findAllByRank();
         this.ranker = new Ranker(temp);
     }
 
@@ -38,34 +39,65 @@ public class WebSiteService {
         if (exact) {
             String word = words.substring(1, words.length() - 1);
             List<String> listOfExactWords = List.of(word.split("[,!.+/ ]+"));
-            // List<UnstemmedSamples> tempo= unstemmedSampleRepository.findByWordIn(listOfExactWords);
-            // //we need to match urls with each other 
-            // //then check if indices are consecutive
-            // //then return the list of websites
-            // for(int i=0;i<tempo.size();i++)
-            // {   
-                
-            //     List<URLS> urls = tempo.get(i).getUrls();
-            //     for(int j=0;j<urls.size();j++)
-            //     {
-
+            List<UnstemmedSamples> tempo = unstemmedSampleRepository.findByWordIn(listOfExactWords);
+            // we need to match urls with each other
+            // then check if indices are consecutive
+            // then return the list of websites
+            // ArrayList<UnstemmedSamples> exacList = new ArrayList<UnstemmedSamples>();
+            // List<List<URLS>> urlsList = new ArrayList<List<URLS>>();
+            // for (int i = 0; i < tempo.size(); i++) {
+            // // urlsList.set(i, tempo.get(i).getUrls());
+            // urlsList.add(tempo.get(i).getUrls());
             // }
+            // bool alltrue = false;
+            // ArrayList<Boolean> Flags = new ArrayList<Boolean>(urlsList.size() - 1);//
+            // Boolean array for each
+            // // Here was a for loop from 0->urlsList.size()-1 but i removed it on the
+            // // assumption that if the URL is consistent with the UnstemmedSample, then it
+            // // will be consistent with the rest.
 
+            // for (int j = 1; j < urlsList.size(); j++) {
+            // for (int k = 0; k < urlsList.get(0).size(); k++) {
+            // for (int l = 0; l < urlsList.get(j).size(); l++) {
+            // if (urlsList.get(i).get(k).getUrl().equals(urlsList.get(j).get(l).getUrl()))
+            // {
+            // for (int m = 0; m < urlsList.get(0).get(k).getIndices().size(); m++) {
+            // for (int n = 0; n < urlsList.get(j).get(l).getIndices().size(); n++) {
+            // if (urlsList.get(0).get(k).getIndices().get(m) + j == urlsList.get(j).get(l)
+            // .getIndices().get(n)) {
+            // Flags.set(j - 1, true);// set the condition of indices between 1 and j to
+            // true
+            // }
+            // }
+            // }
+            // for (int test = 0; test < Flags.size(); test++) {
+            // if (Flags.get(test) == true) {
+            // alltrue = true;
+            // } else {
+            // alltrue = false;
+            // break;
+            // }
+            // }
+            // if (alltrue == true) {
+            // exacList.add(urlsList.get(0).get(k).getUrl());
+            // }
+            // }
+            // }
+            // }
+            // }
             return ranker.rank(unstemmedSampleRepository.findByWordIn(listOfExactWords), this.crawlRepo.count(), exact);
         }
-
-
         List<String> listsWords = List.of(words.split("[,!.+/ ]+"));
         List<String> stemmedList = new ArrayList<String>();
         WordProcessor wp = new WordProcessor();
         String stemmedWord;
-       for (int i = 0; i < listsWords.size(); i++) { //for each word in the list
-           //stemmedWord = listsWords.get(i).replaceAll("[^a-zA-Z]", "");
-           stemmedWord = wp.processWord(listsWords.get(i));
-           if (stemmedWord != null && !stemmedWord.equals(""))
+        for (int i = 0; i < listsWords.size(); i++) { // for each word in the list
+            // stemmedWord = listsWords.get(i).replaceAll("[^a-zA-Z]", "");
+            stemmedWord = wp.processWord(listsWords.get(i));
+            if (stemmedWord != null && !stemmedWord.equals(""))
                 stemmedList.add(stemmedWord);
-               
-       }
+
+        }
         return ranker.rank(sampleRepo.findByWordIn(stemmedList), this.crawlRepo.count());
     }
 
